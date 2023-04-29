6691829570fe3130ae2109c1fcc65472b7a11f05
Tushar Kuchekar <tushar.kuchekar@inspirebrands.com>
2023-04-29 12:40:28 +0530

    code added

--- app.py
+++ app.py
@@ -1,28 +1,20 @@
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Define a dictionary to store names and their corresponding messages
-messages = {}
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/", methods=["GET"])
-def hello():
-    return "Hello, I love you Riya!"
-
-@app.route("/messages", methods=["GET", "POST"])
-def get_or_create_message():
-    if request.method == "GET":
-        # Return all messages as a JSON response
-        return jsonify(messages)
-    elif request.method == "POST":
-        # Create a new message
-        name = request.json.get("name")
-        message = request.json.get("message")
-        if not name or not message:
-            return jsonify({"error": "Both 'name' and 'message' fields are required."}), 400
-        messages[name] = message
-        return jsonify({"success": f"Message for '{name}' created successfully."}), 201
+@app.route('/answer', methods=['POST'])
+def answer():
+    answer = request.form['answer']
+    if answer == 'YES':
+        return "I love you so much"
+    elif answer == 'NO':
+        return "Sorry to say that, You missed an opportunity !!"
+    else:
+        return "Invalid answer"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
