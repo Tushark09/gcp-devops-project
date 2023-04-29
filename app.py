1785280b36b5032ab164fbbdf52afac9d094c5c6
Tushar Kuchekar <tushar.kuchekar@inspirebrands.com>
2023-04-29 11:29:09 +0530

    app version changed for dev

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, This is Tushar!"
+    return "Hello, I love you Riya!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
