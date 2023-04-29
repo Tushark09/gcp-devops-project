ffca7d592d7663e38b3ffff8153abb1f41d64a32
Tushar Kuchekar <tushar.kuchekar@inspirebrands.com>
2023-04-29 12:58:01 +0530

    code added

--- app.py
+++ app.py
@@ -3,18 +3,22 @@ from flask import Flask, render_template, request
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 @app.route('/answer', methods=['POST'])
 def answer():
     answer = request.form['answer']
-    if answer == 'YES':
-        return "I love you so much"
-    elif answer == 'NO':
+    if answer == 'yes':
+        return "I love you so much!"
+    elif answer == 'no':
         return "Sorry to say that, You missed an opportunity !!"
+    elif answer == 'maybe':
+        return "Let's see how things go!"
+    elif answer == 'not sure':
+        return "Take your time and think it over!"
     else:
-        return "Invalid answer"
+        return "I didn't understand your answer."
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
